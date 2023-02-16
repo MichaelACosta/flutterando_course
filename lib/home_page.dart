0ad9156e5c205bf10c214f49c8733ff648e535b7
Michael Costa <macosta@inf.ufpel.edu.br>
2023-02-16 15:10:10 -0300

    using StatefulWidget
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,28 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/home_controller.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
-  Widget build(BuildContext context){
+  State<HomePage> createState() => _HomePageState();
+}
 
-    var controller = HomeController.of(context);
-    final size = MediaQuery.of(context).size;
-    final navigation = Navigator.of(context);
-    final theme = Theme.of(context);
-
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+  
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: const Text('Home'),
       ),
       body: Center(
-          child: Text('Flutterando ${controller.value}'),
+          child: Text('Flutterando $counter'),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          controller.increment();
+          setState(() {
+            counter++;
+          });
         },
       ),
     );
