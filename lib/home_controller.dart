792cb8f5231ca5e4b5dd6ebc839144046bd19520
Michael Costa <macosta@inf.ufpel.edu.br>
2023-02-16 13:56:06 -0300

    using InheritedWidget
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

---
+++ lib/home_controller.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeController extends InheritedWidget{
+  HomeController({Key? key, required Widget child})
+    : super(
+      key: key,
+      child: child,
+    );
+
+  int value = 0;
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget){
+    return false;
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/home_controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
+
+    var controller = context.dependOnInheritedWidgetOfExactType<HomeController>()!;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
       body: Center(
-          child: Text('Flutterando'),
+          child: Text('Flutterando ${controller.value}'),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         onPressed: () {
           print('click');
         },
